7d01799bea477a0270fd9a280dc6d2b45c82f598
albertocanoroca <alberto.canoroca@cieep.com>
2023-10-26 12:17:15 +0200

    Initial Commit

---
+++ Ejercicio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by Alberto Cano Roca on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
